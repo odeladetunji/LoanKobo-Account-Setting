36d72ed32496b4e18eda915d376a1ff06bba4416
tobioyelami <toyelami@teamapt.com>
2019-10-17 13:55:58 +0100

    Fixed bug

--- src/main/java/com/entity/Biodata.java
+++ src/main/java/com/entity/Biodata.java
@@ -1,14 +1,13 @@
 package com.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
 public class Biodata {
 
     @Id
